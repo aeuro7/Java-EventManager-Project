8deba695746bc7b78bb1acfa7e3230cba40bc187
euro <tibet.c@ku.th>
2023-08-03 09:55:09 +0700

    1

---
+++ src/main/java/cs211/project/cs211661project/HelloApplication.java
@@ -0,0 +1,30 @@
+package cs211.project.cs211661project;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import cs211.project.services.FXRouter;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        configRoute();
+
+        FXRouter.bind(this, stage, "CS211 661 Project");
+        FXRouter.goTo("main-menu");
+    }
+
+    private static void configRoute() {
+        String resourcesPath = "cs211/project/views/";
+        FXRouter.when("hello", resourcesPath + "hello-view.fxml");
+        FXRouter.when("main-menu", resourcesPath + "main-menu.fxml");
+        FXRouter.when("login-view", resourcesPath + "login-view.fxml");
+
+    }
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
--- src/main/java/cs211/project/services/DataSource.java
+++
@@ -1,6 +0,0 @@
-package cs211.project.services;
-
-public interface DataSource<Text> {
-    Text readData();
-    void writeData(Text text);
-}
