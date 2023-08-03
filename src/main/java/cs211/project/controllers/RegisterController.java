4b0ebb64f963525598555f2e4d0fafdc56633ed7
nammeyyy <nutpawee.k@ku.th>
2023-08-03 10:24:17 +0700

    Register Page 0.1

---
+++ src/main/java/cs211/project/controllers/RegisterController.java
@@ -0,0 +1,24 @@
+package cs211.project.controllers;
+
+import cs211.project.services.FXRouter;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
+public class RegisterController {
+    @FXML private TextField nameTextField;
+    @FXML private TextField usernameTextField;
+    @FXML private TextField emailTextField;
+    @FXML private TextField passwordTextField;
+    @FXML private TextField conPasswordTextField;
+
+    @FXML
+    public void onSignUpButtonClick() {
+        try {
+            FXRouter.goTo("hello");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
