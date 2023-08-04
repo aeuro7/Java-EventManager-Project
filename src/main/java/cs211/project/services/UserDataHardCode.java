3c6351fdd1d4d97c1d69bc2fb7093493596dc69c
aphisit prasertvesyakorn <aphisit.pra@ku.th>
2023-08-04 20:39:30 +0700

    return of the old file and pls dont rebase again!

---
+++ src/main/java/cs211/project/services/UserDataHardCode.java
@@ -0,0 +1,27 @@
+package cs211.project.services;
+
+import cs211.project.models.User;
+import cs211.project.models.UserList;
+
+import java.util.ArrayList;
+
+public class UserDataHardCode implements DataSource {
+    private UserList userList;
+    public UserDataHardCode() {
+        userList = new UserList();
+    }
+    @Override
+    public UserList readData() {
+        User newUser = new User("johny001", "John_lnw_za", "2568");
+        User newUser2 = new User("baiboy001", "Big_bro", "1111");
+        User newUser3 = new User("humnoi112", "Big_Dig", "5555");
+        userList.addUser(newUser);
+        userList.addUser(newUser2);
+        userList.addUser(newUser3);
+        return userList;
+    }
+    @Override
+    public void writeData(Object o) {
+
+    }
+}
