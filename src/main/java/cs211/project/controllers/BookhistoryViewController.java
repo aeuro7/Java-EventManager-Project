862be3d3aa37024bb8efdbb102437cfe03242255
euro <tibet.c@ku.th>
2023-08-23 12:35:48 +0700

    add show where you are in booking-view

--- src/main/java/cs211/project/controllers/BookhistoryViewController.java
+++ src/main/java/cs211/project/controllers/BookhistoryViewController.java
@@ -1,21 +1,39 @@
 package cs211.project.controllers;
 
+import cs211.project.models.UserList;
+import cs211.project.services.DataSource;
 import cs211.project.services.FXRouter;
+import cs211.project.services.UserDataHardCode;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.control.Label;
+import javafx.scene.shape.Rectangle;
 
 import java.io.IOException;
 
 public class BookhistoryViewController {
 
+    @FXML private Group nowOn;
+    @FXML private Group nowCom;
+
+
+
     @FXML
-    public void logoutButton() {
+    private void initialize() {
+        nowCom.setVisible(false);
+        nowOn.setVisible(false);
+
+    }
+
+    @FXML
+    private void logoutButton() {
         try {
             FXRouter.goTo("login-view");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-    @FXML public void gotoMainMenu() {
+    @FXML private void gotoMainMenu() {
         try {
             FXRouter.goTo("main-menu");
         } catch (IOException e) {
@@ -30,5 +48,21 @@ public class BookhistoryViewController {
             throw new RuntimeException(e);
         }
     }
+
+    @FXML private void showOngoing() {
+            nowOn.setVisible(true);
+            nowCom.setVisible(false);
+
+
+    }
+
+    @FXML private void showCompleted() {
+        nowCom.setVisible(true);
+        nowOn.setVisible(false);
+
+
+    }
+
+
 }
 
