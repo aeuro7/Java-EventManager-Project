83fd47f6c88080f56e94b7131b2968f29669f098
nammeyyy <nutpawee.k@ku.th>
2023-08-23 12:41:27 +0700

    add Event and EventList

---
+++ src/main/java/cs211/project/models/EventList.java
@@ -0,0 +1,28 @@
+package cs211.project.models;
+
+import java.util.ArrayList;
+
+public class EventList {
+    private ArrayList<Event> eventList;
+
+    public EventList() {
+        eventList = new ArrayList<>();
+    }
+
+    public void addEvent(Event event) {
+        eventList.add(event);
+    }
+
+    public ArrayList<Event> getAllEvent() {
+        return eventList;
+    }
+
+    public boolean isThisEventExist(String Event) {
+        for(Event event: eventList) {
+            if(event.getEventName().equals(Event)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
