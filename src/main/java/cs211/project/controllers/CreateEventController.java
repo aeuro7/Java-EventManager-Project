97b339fc194df6fa44a920c6474594e86b6444e4
nammeyyy <nutpawee.k@ku.th>
2023-08-29 15:24:18 +0700

    update reg&event

--- src/main/java/cs211/project/controllers/CreateEventController.java
+++ src/main/java/cs211/project/controllers/CreateEventController.java
@@ -2,23 +2,29 @@ package cs211.project.controllers;
 
 import cs211.project.services.FXRouter;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 
 public class CreateEventController {
+    @FXML private TextField eventnameTextField;
+    @FXML private TextField startDateTextField;
+    @FXML private TextField startTimeTextField;
+    @FXML private TextField dueDateTextField;
+    @FXML private TextField dueTimeTextField;
+    @FXML private TextField locationTextField;
+    @FXML private TextField audienceTextField;
+    @FXML private TextField limitStaffTextField;
+    @FXML private TextField descriptionTextField;
+    @FXML public void initialize() {
+
+    }
     @FXML
     public void browsepicBotton() {
 
     }
 
     @FXML public void createEventButton() {
-        try {
-            FXRouter.goTo("main-menu");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    @FXML public void cancelCreateEventButton() {
         try {
             FXRouter.goTo("main-menu");
         } catch (IOException e) {
@@ -40,5 +46,11 @@ public class CreateEventController {
             throw new RuntimeException(e);
         }
     }
-
+    @FXML private void logoutButton() {
+        try {
+            FXRouter.goTo("login-view");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/main/java/cs211/project/controllers/RegisterController.java
+++ src/main/java/cs211/project/controllers/RegisterController.java
@@ -5,6 +5,7 @@ import cs211.project.models.UserList;
 import cs211.project.services.DataSource;
 import cs211.project.services.FXRouter;
 import cs211.project.services.UserDataHardCode;
+import cs211.project.services.UserDataSource;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -12,42 +13,48 @@ import javafx.scene.control.TextField;
 import java.io.IOException;
 
 public class RegisterController {
+    @FXML private TextField accountnameTextField;
     @FXML private TextField usernameTextField;
     @FXML private TextField passwordTextField;
     @FXML private TextField conPasswordTextField;
+    @FXML private Label accountnameErrorLabel;
     @FXML private Label usernameErrorLabel;
     @FXML private Label passwordErrorLabel;
     @FXML private Label cPasswordErrorLabel;
 
     private UserList userList;
     DataSource<UserList> dataSource;
-
-    public RegisterController() {
-        dataSource = new UserDataHardCode(); // Initialize your data source here
-    }
     @FXML
     public void initialize() {
         // Hide error labels when the controller is initialized
+        accountnameErrorLabel.setVisible(false);
         usernameErrorLabel.setVisible(false);
         passwordErrorLabel.setVisible(false);
         cPasswordErrorLabel.setVisible(false);
-        dataSource = new UserDataHardCode();
+        dataSource = new UserDataSource("data", "login.csv");
         userList = dataSource.readData();
     }
 
     @FXML
     public void signUpButtonClick() {
         // Get user input from text fields
+        String accountname = accountnameTextField.getText();
         String username = usernameTextField.getText();
         String password = passwordTextField.getText();
         String confirmPassword = conPasswordTextField.getText();
 
         // Perform validation checks
         boolean isValid = false;
+        if(!accountname.isEmpty()) {
+            accountnameErrorLabel.setVisible(false);
+        } else{
+            accountnameErrorLabel.setVisible(true);
+        }
 
-        if (username.isEmpty()) {
+        if (!username.isEmpty()) {
             // check if usernames is already existed?
             if(!userList.isUserNameExists(username)) {
+                usernameErrorLabel.setVisible(false);
                 isValid = true;
             } else {
                 usernameErrorLabel.setVisible(true);
@@ -68,7 +75,7 @@ public class RegisterController {
         if (isValid) {
             // Perform the signup process here
             // For example, create a new User object and save it to a database
-            User newUser = new User(username, "AccountName", password);
+            User newUser = new User(username, accountname, password);
             userList.addUser(newUser);
             dataSource.writeData(userList);
 
@@ -84,8 +91,12 @@ public class RegisterController {
     public void checkUsernameClick() {
         String username = usernameTextField.getText();
 
-        if (userList.isUserNameExists(username)) {
-            usernameErrorLabel.setVisible(false);
+        if (!username.isEmpty()) {
+            if(!userList.isUserNameExists(username)) {
+                usernameErrorLabel.setVisible(false);
+            } else {
+                usernameErrorLabel.setVisible(true);
+            }
         } else {
             usernameErrorLabel.setVisible(true);
         }
