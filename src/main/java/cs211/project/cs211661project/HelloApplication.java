7ec3b4c5a770353e83d018b022af0f227867715f
bvnnal <bamapinya2503@gmail.com>
2023-08-29 14:22:49 +0700

    QC Test

--- src/main/java/cs211/project/cs211661project/HelloApplication.java
+++ src/main/java/cs211/project/cs211661project/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
         configRoute();
 
         FXRouter.bind(this, stage, "CS211 661 Project");
-        FXRouter.goTo("event-view");
+        FXRouter.goTo("login-view");
 
     }
 
