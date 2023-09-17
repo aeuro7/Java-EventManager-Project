bdafb7a1bbf3cff2e4c6d771dec58c6ab7ffc188
euro <tibet.c@ku.th>
2023-09-17 22:42:24 +0700

    add table view 3 dff fxml

--- src/main/java/cs211/project/controllers/BookhistoryViewController.java
+++ src/main/java/cs211/project/controllers/BookhistoryViewController.java
@@ -1,24 +1,82 @@
 package cs211.project.controllers;
 
+import cs211.project.models.Event;
+import cs211.project.models.EventList;
+import cs211.project.models.team.Team;
+import cs211.project.models.team.TeamList;
 import cs211.project.services.FXRouter;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
 import javafx.scene.Group;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class BookhistoryViewController {
 
     @FXML private Group nowOn;
     @FXML private Group nowCom;
+    @FXML private TableView eventTableView;
+
+    private EventList eventList;
 
     @FXML
     private void initialize() {
         nowCom.setVisible(false);
         nowOn.setVisible(false);
+        showTable(eventList);
+    }
+
+    private void showTable(EventList eventList) {
+        TableColumn<Event, String> eventNameColumn = new TableColumn<>("Event Name");
+        eventNameColumn.setCellValueFactory(new PropertyValueFactory<>("eventName"));
+
+        TableColumn<Event, String> startTimeColumn = new TableColumn<>("Start From");
+        startTimeColumn.setCellValueFactory(cellData -> {
+            long startTime = cellData.getValue().getStartTime();
+            String formattedTimestamp = formatTimestamp(startTime); // Format the timestamp
+            return new SimpleStringProperty(formattedTimestamp);
+        });
+
+        TableColumn<Event, String> dueTimeColumn = new TableColumn<>("End At");
+        dueTimeColumn.setCellValueFactory(cellData -> {
+            long endTime = cellData.getValue().getDueTime();
+            String formattedTimestamp = formatTimestamp(endTime);
+            return new SimpleStringProperty(formattedTimestamp);
+        });
+
+        TableColumn<Event, String> leftSeatNewColumn = new TableColumn<>("Seat Available");
+        leftSeatNewColumn.setCellValueFactory(new PropertyValueFactory<>("leftSeat"));
+
+        TableColumn<Event, String> locationNewColumn = new TableColumn<>("Location");
+        locationNewColumn.setCellValueFactory(new PropertyValueFactory<>("location"));
+
+        eventTableView.getColumns().clear();
+
+        eventTableView.getColumns().add(eventNameColumn);
+        eventNameColumn.setMinWidth(180);
+
+        eventTableView.getColumns().add(startTimeColumn);
+        eventTableView.getColumns().add(dueTimeColumn);
+
+        eventTableView.getColumns().add(leftSeatNewColumn);
+        leftSeatNewColumn.setMinWidth(30);
+        eventTableView.getColumns().add(locationNewColumn);
+        locationNewColumn.setMinWidth(184);
+
+        eventTableView.getItems().clear();
+
+//        for (Event event : eventList.getAllEvent()) {
+//            eventTableView.getItems().add(event);
+//        }
 
     }
 
-    @FXML
+        @FXML
     private void logoutButton() {
         try {
             FXRouter.goTo("login-view");
@@ -77,7 +135,10 @@ public class BookhistoryViewController {
 
     }
 
-
+    private String formatTimestamp(long timestamp) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return dateFormat.format(new Date(timestamp));
+    }
 
 
 }
