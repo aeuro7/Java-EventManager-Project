ca85c5c9cd10cce45dba7f2e3883d4472d34db6d
bvnnal <bamapinya2503@gmail.com>
2023-09-18 14:19:11 +0700

    Connect page

---
+++ src/main/java/cs211/project/controllers/ManageTeamViewController.java
@@ -0,0 +1,89 @@
+package cs211.project.controllers;
+
+import cs211.project.models.team.Team;
+import cs211.project.models.team.TeamList;
+import cs211.project.services.FXRouter;
+import cs211.project.services.TeamDataSource;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.io.IOException;
+
+public class ManageTeamViewController {
+    @FXML private Label teamNameLabel;
+    @FXML private Label eventNameLabel;
+    @FXML private Team selectTeam;
+
+    @FXML
+    private TableView<Team> teamTableView;
+
+    private TeamList teamList;
+
+    @FXML
+    private void initialize() {
+
+    }
+
+    private void showTable(TeamList teamList) {
+        TableColumn<Team, String> nameTeamColumn = new TableColumn<>("Team Name");
+        nameTeamColumn.setCellValueFactory(new PropertyValueFactory<>("nameTeam"));
+
+        TableColumn<Team, String> eventIDColumn = new TableColumn<>("Event ID");
+        eventIDColumn.setCellValueFactory(new PropertyValueFactory<>("eventID"));
+
+        teamTableView.getColumns().clear();
+        teamTableView.getColumns().addAll(nameTeamColumn, eventIDColumn);
+
+        teamTableView.getItems().clear();
+
+        for (Team team: teamList.getAllTeams()) {
+            teamTableView.getItems().add(team);
+        }
+    }
+
+    @FXML
+    private void logoutButton() {
+        try {
+            FXRouter.goTo("login-view");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @FXML private void gotoMainMenu() {
+        try {
+            FXRouter.goTo("main-menu");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @FXML private void gotoEditprofile() {
+        try {
+            FXRouter.goTo("profile-view");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    } @FXML private void gotoBook() {
+        try {
+            FXRouter.goTo("book-view");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @FXML private void gotoManageTeam() {
+        try {
+            FXRouter.goTo("manage-team");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @FXML private void gotoManageEvent() {
+        try {
+            FXRouter.goTo("manage-event");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
