f961bd399523ea999368b9c3a7463b7c3adc81eb
euro <tibet.c@ku.th>
2023-09-20 13:14:15 +0700

    fix team/calendar models

--- src/main/java/cs211/project/models/Calendar.java
+++ src/main/java/cs211/project/models/Calendar.java
@@ -2,20 +2,22 @@ package cs211.project.models;
 
 public class Calendar {
     private String calendarName;
-    private String eventLinkName;
+    private String eventID;
+    private String faction;
     private long startTime;
     private long dueTime;
     private String detail;
 
-    public Calendar(String name, String event, long startTime, long dueTime) {
+    public Calendar(String name, String event, String faction, long startTime, long dueTime) {
         this.calendarName = name;
-        this.eventLinkName = event;
+        this.eventID = event;
+        this.faction = faction;
         this.startTime = startTime;
         this.dueTime = dueTime;
         this.detail = "";
     }
-    public Calendar(String name, String event, long startTime, long dueTime, String info) {
-        this(name, event, startTime, dueTime);
+    public Calendar(String name, String event, String faction, long startTime, long dueTime, String info) {
+        this(name, event, faction, startTime, dueTime);
         this.detail = info;
     }
 
@@ -23,8 +25,11 @@ public class Calendar {
         return calendarName;
     }
 
-    public String getEventLinkName() {
-        return eventLinkName;
+    public String getEventID() {
+        return eventID;
+    }
+    public String getFaction() {
+        return faction;
     }
 
     public long getStartTime() {
@@ -42,19 +47,15 @@ public class Calendar {
     public void setCalendarName(String calendarName) {
         this.calendarName = calendarName;
     }
-
-    public void setEventLinkName(String eventLinkName) {
-        this.eventLinkName = eventLinkName;
+    public void setFaction(String faction) {
+        this.faction = faction;
     }
-
     public void setStartTime(long startTime) {
         this.startTime = startTime;
     }
-
     public void setDueTime(long dueTime) {
         this.dueTime = dueTime;
     }
-
     public void setDetail(String detail) {
         this.detail = detail;
     }
